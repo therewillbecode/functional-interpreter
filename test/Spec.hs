ec229589a3aebde6068277887220b67d425e0b67
therewillbecode <tomw08@gmail.com>
2019-05-01 07:44:43 +0100

    Add Step1 Unit Test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,24 @@
 import Test.Hspec
 
+import qualified Data.Map.Lazy as M
+import Data.Map.Lazy (Map)
+
+import Interpreter
+
 main :: IO ()
-main =
-  hspec $ do
-    describe "Exercise C" $ do
-      it "returns the original number when given a positive input" $
-        1 `shouldBe` 2
+main = hspec $ step1 -- >> step2
+
+step1 =
+  describe "Step 1" $
+  it "Eval can add two variables in memory" $ eval varMap expr `shouldBe` 65
+  where
+    varMap = M.fromList [("x", 42), ("y", 23)]
+    expr = Add (Variable "x") (Variable "y")
+--step2 =
+--  describe "Step 2" $ 
+--    it "Eval can add variable in memory and a constant" $
+--      eval varMap expr `shouldBe` 65
+--  where
+--    varMap = M.empty
+--    expr = Let "x" 42 (Add (Variable x) (Number 23))
+--
