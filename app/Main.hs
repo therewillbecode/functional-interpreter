00019ddc6a4508422c60d41bc83ce9a37120f131
therewillbecode <tomw08@gmail.com>
2019-05-04 15:19:50 +0200

    Added Example Language Expressions to Main

--- app/Main.hs
+++ app/Main.hs
@@ -9,11 +9,13 @@ import Data.Map.Lazy (Map)
 import Interpreter
 
 main :: IO ()
-main = print $ runExcept $ evalStateT (eval expr) emptyMemory
+main = print $ runExcept $ evalStateT (eval applyLambdaExp) emptyMemory
   where
-    expr =
+    readBoundVariables = Add (Variable "x") (Variable "y")
+    bindVariableExp = Let "x" (Number 42) (Add (Variable "x") (Number 23)) -- 65
+    applyLambdaExp =
       Let
         "f"
         (Lambda "x" (Add (Variable "x") (Variable "x")))
-        (FunCall (Variable "f") [Number 21])
+        (FunCall (Variable "f") (Number 21))
      -- 42
--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -27,20 +27,36 @@ data Exp
         Exp
         Exp -- let name = exp in exp
   | FunCall Exp
-            [Exp]
+            Exp
   | Lambda String
            Exp
   deriving (Show)
 
 data Value
   = NumVal Int
-  | FunVal (Exp -> StateT Memory (Except Err) Value)
+  | FunVal (Exp -> StateT Memory (Except LangErr) Value)
 
 instance Show Value where
   show (FunVal _) = "FunVal String -> Value"
   show (NumVal n) = show n
 
-type Err = String
+data LangErr =
+  LangErr Err
+          (Maybe Value)
+
+instance Show LangErr where
+  show (LangErr err val) = show err ++ " in the expression: \n" ++ show val
+
+data Err
+  = UnboundVar String
+  | TypeError TypeErr
+  | VarAlreadyBound String
+  deriving (Show)
+
+data TypeErr
+  = ExpectedFunction
+  | ExpectedNumVal
+  deriving (Show)
 
 {-
 data LangErr = {
@@ -83,47 +99,45 @@ Solonarv
          liftIO . print =<< asks (M.! "x")
 Solonarv: 3; 42; 3
 -}
-eval :: Exp -> StateT Memory (Except Err) Value
+eval :: Exp -> StateT Memory (Except LangErr) Value
 eval (Number n) = return $ NumVal n
 eval (Variable name) = lookupVarBinding name
 eval (Add a b) = binaryOp add a b
 eval (Mul a b) = binaryOp mul a b
 eval (Let varName varExp exp) = bindVar varName varExp >> eval exp
 eval (Lambda paramName bodyExp) = defineLambda paramName bodyExp
-eval (FunCall funcName [arg]) = do
+eval (FunCall funcName arg) = do
   func <- eval funcName
   apply func arg
 
-defineLambda :: String -> Exp -> StateT Memory (Except Err) Value
+defineLambda :: String -> Exp -> StateT Memory (Except LangErr) Value
 defineLambda paramName body =
   return $ FunVal $ \exp -> eval (Let paramName exp body)
 
-apply :: Value -> Exp -> StateT Memory (Except Err) Value
+apply :: Value -> Exp -> StateT Memory (Except LangErr) Value
 apply (FunVal func) arg = func arg
-apply val _ =
-  throwError $
-  "TypeError - Not a function: \n" ++ show val ++ " is not a function"
+apply val _ = throwError $ LangErr (TypeError ExpectedFunction) (pure val)
 
 binaryOp ::
-     (Value -> Value -> Either Err Value)
+     (Value -> Value -> Either LangErr Value)
   -> Exp
   -> Exp
-  -> StateT Memory (Except Err) Value
+  -> StateT Memory (Except LangErr) Value
 binaryOp op a b = do
   valA <- eval a
   valB <- eval b
   either throwError return (op valA valB)
 
-lookupVarBinding :: String -> StateT Memory (Except Err) Value
+lookupVarBinding :: String -> StateT Memory (Except LangErr) Value
 lookupVarBinding name = do
   (Memory mem) <- get
   case M.lookup name mem of
     (Just varBinding) -> return varBinding
-    Nothing -> throwError ("Unbound Variable: " ++ name)
+    Nothing -> throwError $ LangErr (UnboundVar name) Nothing
   where
     mem = M.fromList [("x", 42), ("y", 23)]
 
-bindVar :: String -> Exp -> StateT Memory (Except Err) VarBinding
+bindVar :: String -> Exp -> StateT Memory (Except LangErr) VarBinding
 bindVar name exp = do
   val <- eval exp
   modify (\(Memory m) -> Memory $ M.insert name val m)
@@ -132,10 +146,12 @@ bindVar name exp = do
 getBindingVal :: VarBinding -> Value
 getBindingVal (VarBinding _ val) = val
 
-add :: Value -> Value -> Either Err Value
+add :: Value -> Value -> Either LangErr Value
 add (NumVal a) (NumVal b) = Right $ NumVal $ a + b
-add _ _ = Left "typeError - Only NumVals can be added"
+add a (NumVal _) = Left $ LangErr (TypeError ExpectedNumVal) (pure a)
+add (NumVal _) b = Left $ LangErr (TypeError ExpectedNumVal) (pure b)
 
-mul :: Value -> Value -> Either Err Value
+mul :: Value -> Value -> Either LangErr Value
 mul (NumVal a) (NumVal b) = Right $ NumVal $ a * b
-mul _ _ = Left "typeError - Only NumVals can be multiplied"
+mul a (NumVal _) = Left $ LangErr (TypeError ExpectedNumVal) (pure a)
+mul (NumVal _) b = Left $ LangErr (TypeError ExpectedNumVal) (pure b)
